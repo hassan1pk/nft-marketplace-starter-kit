be741b348db81348ef6e5d96b192f204f93d81e9
Hassan Tariq <hassan1pk@hotmail.com>
2022-11-10 00:16:42 +0500

    End of section 22

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
 import KryptoBird from "../abis/KryptoBird.json";
+//import Contract from "web3-eth-contract";
 
 class App extends Component {
   async componentDidMount() {
@@ -46,13 +47,45 @@ class App extends Component {
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
-    console.log(accounts);
+    this.setState({ account: accounts });
+    //console.log(this.state.account);
+    /*const networkId = await window.web3.eth.net.getId();
+    console.log(networkId);*/
+    const networkId = await window.ethereum.request({ method: "net_version" });
+    console.log(networkId);
+    const networkData = KryptoBird.networks[networkId];
+    if (networkData) {
+      const abi = KryptoBird.abi;
+      const address = networkData.address;
+      //const contract = new Contract(abi, address);
+      const contract = new window.web3.eth.Contract(abi, address);
+      console.log(contract);
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+    };
   }
 
   render() {
     return (
       <div>
-        <h1>NFT Marketplace</h1>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <div
+            className="navbar-brand col-sm-3 col-md-3 mr-0"
+            style={{ color: "white" }}
+          >
+            Krypto Birdz (NFT) non-fungible token
+          </div>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"> {this.state.account}</small>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
